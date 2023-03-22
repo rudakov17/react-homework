e7d18a6603b964cb006ceee68861cff88aaf2850
rudakov17 <110357201+rudakov17@users.noreply.github.com>
2023-03-22 19:39:30 +0300

    change setstate

--- 001-my-counter/src/components/ButtonReset.js
+++ 001-my-counter/src/components/ButtonReset.js
@@ -1,9 +1,5 @@
-function ButtonReset({ count, onClick }) {
-  return (
-    <button onClick={() => onClick((count = 0))}>
-      давай по новой, всё хуйня
-    </button>
-  );
+function ButtonReset({ onClick }) {
+  return <button onClick={() => onClick(0)}>давай по новой, всё хуйня</button>;
 }
 
 export default ButtonReset;
